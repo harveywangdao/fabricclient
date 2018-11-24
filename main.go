0cebc63a46af4f164412f563162ebfcd30ebcb8d
thomas <1272777053@qq.com>
2018-11-24 11:09:48 +0800

    delete log

--- main.go
+++ main.go
@@ -16,24 +16,26 @@ const (
 )
 
 func initLogger() error {
-	st, err := os.Stat(logDirPath)
-	if err == nil {
-		if !st.IsDir() {
-			log.Fatal(logDirPath, "is not dir")
-		}
-	} else {
-		if os.IsNotExist(err) {
-			err = os.Mkdir(logDirPath, os.ModePerm)
-			if err != nil {
-				log.Fatal("mkdir fail")
+	/*	st, err := os.Stat(logDirPath)
+		if err == nil {
+			if !st.IsDir() {
+				log.Fatal(logDirPath, "is not dir")
 			}
 		} else {
-			log.Fatal(logDirPath, "error")
+			if os.IsNotExist(err) {
+				err = os.Mkdir(logDirPath, os.ModePerm)
+				if err != nil {
+					log.Fatal("mkdir fail")
+				}
+			} else {
+				log.Fatal(logDirPath, "error")
+			}
 		}
-	}
 
-	fileHandler := logger.NewFileHandler(logFilePath)
-	logger.SetHandlers(logger.Console, fileHandler)
+		fileHandler := logger.NewFileHandler(logFilePath)
+		logger.SetHandlers(logger.Console, fileHandler)
+	*/
+	logger.SetHandlers(logger.Console)
 	//defer logger.Close()
 	logger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	logger.SetLevel(logger.INFO)
